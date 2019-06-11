538dcd9521931719fdb9325c6e233a569d2792a9
sl4shbored <39769731+sl4shbored@users.noreply.github.com>
2019-06-11 15:54:06 +0200

    changed text

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Bla!\n');
+  res.send('Azure Pipelines\n');
 });
 
 var port = process.env.PORT||PORT;
