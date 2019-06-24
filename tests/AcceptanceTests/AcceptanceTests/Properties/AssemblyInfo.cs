9aea9ab052ea2b96841111b772cc76f052d2655f
Matthias <matthias1992reitz@t-online.de>
2019-06-24 10:45:18 +0200

    added second round of acceptance tests

---
+++ tests/AcceptanceTests/AcceptanceTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AcceptanceTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AcceptanceTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e86582fb-2111-4de2-968b-83c57a7bdb29")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tests/AcceptanceTests/AcceptanceTests/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace UITests
+{
+    [TestClass]
+    public class UITest
+    {
+        public UITest()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.Navigate().GoToUrl("https://devcicdonazure.azurewebsites.net/");
+
+            IWebElement anzeigeText = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.XPath("/html/body")));
+
+            try
+            {
+                System.Threading.Thread.Sleep(5000);
+                Assert.AreEqual(anzeigeText.Text, "Azure Pipelines");
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
