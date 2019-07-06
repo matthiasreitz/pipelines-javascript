e5580746d36adf06d17a5790665166aa3f0feedc
Matthias <matthias1992reitz@t-online.de>
2019-07-06 16:06:21 +0200

    weather app

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
-'use strict';
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
 
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
+const apiKey = '98b7ad2de9e2f8753f688f25614207f0';
 
-// App
-const app = express();
-app.get('/', (req, res) => {
-  res.send(process.env.AZURENAME);
-});
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
+
+app.get('/', function (req, res) {
+  res.render('index', {weather: null, error: null});
+})
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
 
 var port = process.env.PORT||PORT;
 app.listen(port);
